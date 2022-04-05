628881355c6ff67fbe203fa234d1c810f56910ce
manasvinis23 <87264894+manasvinis23@users.noreply.github.com>
2022-04-05 20:58:33 +0530

    update

---
+++ New folder/script.js
@@ -0,0 +1,21 @@
+var names=new Array();
+names[0]="Yaakov";
+names[1]="John";
+names[2]="Jen";
+names[3]="Jason";
+names[4]="paul";
+names[5]="frank";
+names[6]="larry";
+names[7]="paula";
+names[8]="laura";
+names[9]="jim";
+
+//console.log(names[0].charAT(0));
+for(var i=0;i<names.length;i++){
+    if(names[0].charAT==='J'|| names[0].charAT==='j'){
+    console.log("Goodbye"+ names[i])
+    }
+    else{
+        console.log("Hello"+names[i])
+    }
+}
